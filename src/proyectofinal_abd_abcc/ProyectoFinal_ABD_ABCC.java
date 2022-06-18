40550a6e6a35d1d91c4996d4c54c2b4d9619e29e
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2022-06-18 17:57:12 -0500

    Se creó la ventana principal

---
+++ src/proyectofinal_abd_abcc/ProyectoFinal_ABD_ABCC.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectofinal_abd_abcc;
+
+import vista.ventanaPrincipal;
+
+/**
+ *
+ * @author denis
+ */
+public class ProyectoFinal_ABD_ABCC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ventanaPrincipal().setVisible(true);
+            }
+        });
+    }
+    
+}
