7265e3373c925ad7ea3a9caa02465b9ccaea51f2
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2022-06-18 19:57:41 -0500

    Se agregó código al botón de regresar de la ventana sucursal

--- src/vista/Sucursal.java
+++ src/vista/Sucursal.java
@@ -73,6 +73,11 @@ public class Sucursal extends javax.swing.JFrame {
         jButton5.setText("Limpiar Campos");
 
         jButton6.setText("Regresar");
+        jButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton6ActionPerformed(evt);
+            }
+        });
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Agregar", "Eliminar", "Actualizar", "Consultar" }));
 
@@ -162,6 +167,12 @@ public class Sucursal extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
+        ventanaPrincipal vp = new ventanaPrincipal();
+        this.setVisible(false);
+        vp.setVisible(true);
+    }//GEN-LAST:event_jButton6ActionPerformed
 
     /**
      * @param args the command line arguments
