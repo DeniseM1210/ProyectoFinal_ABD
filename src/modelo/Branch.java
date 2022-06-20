2fecaf06c9c89d7d7ef881e5b7678b3ebb8b582f
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2022-06-19 22:24:55 -0500

    Se añadieron las variables, el constructor, getters y setters en la clase de branch

--- src/modelo/Branch.java
+++ src/modelo/Branch.java
@@ -9,5 +9,57 @@ package modelo;
  * @author denis
  */
 public class Branch {
+    private char branchNo;
+    private String street;
+    private String city;
+    private String postcode;
+
+    public Branch() {
+    }
+
+    public Branch(char branchNo, String street, String city, String postcode) {
+        this.branchNo = branchNo;
+        this.street = street;
+        this.city = city;
+        this.postcode = postcode;
+    }
+
+    public char getBranchNo() {
+        return branchNo;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setBranchNo(char branchNo) {
+        this.branchNo = branchNo;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    @Override
+    public String toString() {
+        return "Branch{" + "branchNo=" + branchNo + ", street=" + street + ", city=" + city + ", postcode=" + postcode + '}';
+    }
+    
     
 }
