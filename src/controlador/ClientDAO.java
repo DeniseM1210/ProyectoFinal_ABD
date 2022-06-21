3dba049a2d5c8fa1a624cbc7cdf9ba49f9dd2461
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2022-06-20 23:09:08 -0500

    Se añadió código para las altas, bajas y cambios en ClientDAO

--- src/controlador/ClientDAO.java
+++ src/controlador/ClientDAO.java
@@ -3,11 +3,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package controlador;
-
+import conexionBD.ConexionBD;
+import modelo.Client;
 /**
  *
  * @author denis
  */
 public class ClientDAO {
+    ConexionBD conexion;
     
+    public ClientDAO(){
+        conexion = new ConexionBD();
+    }
+    
+    public boolean insertarClient(Client c){
+        boolean resultado = false;
+        String sql = "INSERT INTO client VALUES('" + c.getClientNo() +
+                "', '" + c.getfName() + "','" + c.getlName() +
+                "', '" + c.getTelNo() + "','" + c.getPrefType() +
+                "'," + c.getMaxRent() + ",'" + c.getEmail() + "');";
+        resultado = conexion.ejecutarInstruccion(sql);
+        return resultado;
+    }
+    
+    public boolean eliminarClient(char clientNo){
+        boolean resultado = false;
+        String sql = "DELETE FROM client WHERE clientNo = '" + clientNo + "';";
+        resultado = conexion.ejecutarInstruccion(sql);
+        return resultado;
+    }
+    
+    public boolean modificarClient(Client c){
+        boolean resultado = false;
+        String sql = "UPDATE client SET fName = '" + c.getfName() +
+                "', lName = '" + c.getlName() + "', telNo = '" + c.getTelNo() + 
+                        "', prefType = '" + c.getPrefType() + "', maxRent = " +
+                        ", email = '" + c.getEmail() + "';";
+        resultado = conexion.ejecutarInstruccion(sql);
+        return resultado;
+    }
 }
