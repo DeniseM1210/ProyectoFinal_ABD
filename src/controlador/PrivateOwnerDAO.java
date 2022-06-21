cdd1ecf90b3c17c6c9e77f90339c371d41772d1d
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2022-06-20 23:50:05 -0500

    Se añadió código para las altas, bajas y cambios en PrivateOwnerDAO

--- src/controlador/PrivateOwnerDAO.java
+++ src/controlador/PrivateOwnerDAO.java
@@ -4,10 +4,45 @@
  */
 package controlador;
 
+import conexionBD.ConexionBD;
+import modelo.PrivateOwner;
+
 /**
  *
  * @author denis
  */
 public class PrivateOwnerDAO {
+    ConexionBD conexion;
     
+    public PrivateOwnerDAO(){
+        conexion = new ConexionBD();
+    }
+    
+    public boolean insertarPrivateOwner(PrivateOwner po){
+        boolean resultado = false;
+        String sql = "INSERT INTO privateOwner VALUES('" + po.getOwnerNo() +
+                "','" + po.getfName() + "','" + po.getlName() + 
+                "','" + po.getAddress() + "','" + po.getTelNo() +
+                "','" + po.getEmail() + "','" + po.getPassword() + "');";
+        resultado = conexion.ejecutarInstruccion(sql);
+        return resultado;
+    }
+    
+    public boolean eliminarPrivateOwner(char ownerNo){
+        boolean resultado = false;
+        String sql = "DELETE FROM privateOwner WHERE ownerNo = '" + ownerNo + "';";
+        resultado = conexion.ejecutarInstruccion(sql);
+        return resultado;
+    }
+    
+    public boolean modificarPrivateOwner(PrivateOwner po){
+        boolean resultado = false;
+        String sql = "UPDATE privateOwner SET fName = '" + po.getfName() + 
+                "', lName = '" + po.getlName() + "', address = '" + po.getAddress() +
+                "', telNo = '" + po.getTelNo() + "', email = '" + po.getEmail() +
+                "', password = '" + po.getPassword() + "' WHERE ownerNo = '" + po.getOwnerNo() + "';";
+        resultado = conexion.ejecutarInstruccion(sql);
+        
+        return resultado;
+    }
 }
