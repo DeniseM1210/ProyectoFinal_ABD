c157f85a0f6225fad506baf48135f1004dc3bf88
DeniseM1210 <71150096+DeniseM1210@users.noreply.github.com>
2022-06-20 22:40:59 -0500

    Se añadió código para las altas, bajas y cambios en BranchDAO

--- src/controlador/BranchDAO.java
+++ src/controlador/BranchDAO.java
@@ -19,6 +19,26 @@ public class BranchDAO {
     
     public boolean insertarBranch(Branch b){
         boolean resultado = false;
+        String sql = "INSERT INTO branch VALUES('" + b.getBranchNo() + "', '"
+                + b.getStreet() + "','" + b.getCity() + "','" + b.getPostcode() + "');";
+        resultado = conexion.ejecutarInstruccion(sql);
+        return resultado;
+    }
+    
+    public boolean eliminarBranch(char branchNo){
+        boolean resultado = false;
+        String sql = "DELETE FROM branch WHERE branchNo = '" + branchNo + "';";
+        resultado = conexion.ejecutarInstruccion(sql);
+        return resultado;
+    }
+    
+    public boolean modificarBranch(Branch b){
+        boolean resultado = false;
+        String sql = "UPDATE branch SET street = '" + b.getStreet() + 
+                "', city = '" + b.getCity() + "', postcode = '" + b.getPostcode() +
+                "' WHERE branchNo = '" + b.getBranchNo() + "';";
+        resultado = conexion.ejecutarInstruccion(sql);
+        
         return resultado;
     }
 }
